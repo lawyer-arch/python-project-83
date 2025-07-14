79c85fea8ba2d5100280e786a67084ad8e5dfd35
Pavel Anikin <PavelDidko@gmail.com>
2025-07-14 19:26:34 +0300

    настройка ginicorn

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
-app = Flask(__name__)
 
 load_dotenv()
+
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
