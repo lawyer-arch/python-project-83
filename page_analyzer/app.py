75ffc8b2c3b6e4f386df1318a0c1a5ea95e4a0b9
Pavel Anikin <PavelDidko@gmail.com>
2025-07-14 17:45:29 +0300

    Настройка проекта первый шаг

---
+++ page_analyzer/app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def hello():
+    return 'Првиетствую на проекте!'
