006473e42501289b8be4adef651ff69b8e4b6565
Pavel Anikin <PavelDidko@gmail.com>
2025-07-15 13:58:21 +0300

    отладка render и БД

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 make install
 
+echo ">>> Начинаю загрузку database.sql"
 psql -a -d $DATABASE_URL -f database.sql
+echo ">>> Загрузка database.sql завершена"
